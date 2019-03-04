13e4a74116e07246e7555c27c5cef0a42d66952c
masterlopez <javy7@hotmail.com>
2019-03-04 14:06:04 -0400

    Todo list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import TodoItem from "./components/TodoItem";
+import "./components/style.css";
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <div className="todo-list">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+      <br />
       <Footer />
     </div>
   );
---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function TodoItem() {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <p>Text placeholder for checklist item</p>
+    </div>
+  );
+}
+export default TodoItem;
